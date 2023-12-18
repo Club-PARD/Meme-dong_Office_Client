679d472c49e3a33952cadad38ace831a70265e3d
hyungjin0105 <hyungjin015@handong.ac.kr>
2023-12-18 12:59:24 +0900

    first

---
+++ Meme_Dong_Office/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme_Dong_Office
+//
+//  Created by hyungjin kim on 12/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        
+        print("hello world")
+        
+        print("!!!!!!!!hello world")
+
+        print("DD")
+    }
+
+
+}
+
