5404e43f824adabdb78ec4ce2eed08e4342c5dbd
hyungjin0105 <hyungjin015@handong.ac.kr>
2023-12-22 13:56:02 +0900

    MVVM

---
+++ Meme_Dong_Office/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Meme_Dong_Office
+//
+//  Created by hyungjin kim on 12/22/23.
+//
+
+import Foundation
---
+++ Meme_Dong_Office/ViewModel/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  Meme_Dong_Office
+//
+//  Created by hyungjin kim on 12/22/23.
+//
+
+import Foundation
