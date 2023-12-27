d1e982cfdc981071fbbc3c55065fc641f86b6b42
mingseobi <mingseobi@naver.com>
2023-12-27 14:19:50 +0900

    git setting

---
+++ Meme_Dong_Office/Views/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  Meme_Dong_Office
+//
+//  Created by 김민섭 on 12/27/23.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Meme_Dong_Office/Views/TestViewController.swift
@@ -0,0 +1,94 @@
+//
+//  TestViewController.swift
+//  Meme_Dong_Office
+//
+//  Created by 김민섭 on 12/27/23.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    let buttonHeight: CGFloat = 200
+    let spacing: CGFloat = 20
+
+    lazy var button1: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Button 1", for: .normal)
+        button.addTarget(self, action: #selector(button1Tapped(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    lazy var button2: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Button 2", for: .normal)
+        button.addTarget(self, action: #selector(button2Tapped(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    lazy var button3: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Button 3", for: .normal)
+        button.addTarget(self, action: #selector(button3Tapped(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupButtons()
+    }
+
+    func setupButtons() {
+        view.addSubview(button1)
+        view.addSubview(button2)
+        view.addSubview(button3)
+
+        NSLayoutConstraint.activate([
+            button1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button1.topAnchor.constraint(equalTo: view.topAnchor, constant: spacing + 50), // Move down by 50 points
+            button1.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40),
+            button1.heightAnchor.constraint(equalToConstant: buttonHeight)
+        ])
+
+        NSLayoutConstraint.activate([
+            button2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button2.topAnchor.constraint(equalTo: button1.bottomAnchor, constant: spacing),
+            button2.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40),
+            button2.heightAnchor.constraint(equalToConstant: buttonHeight)
+        ])
+
+        NSLayoutConstraint.activate([
+            button3.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button3.topAnchor.constraint(equalTo: button2.bottomAnchor, constant: spacing),
+            button3.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40),
+            button3.heightAnchor.constraint(equalToConstant: buttonHeight)
+        ])
+    }
+
+    @objc func button1Tapped(_ sender: UIButton) {
+        // Handle button 1 tap actions here
+        print("Button 1 tapped")
+        let ViewController = HomeViewController()
+        let navigationController = UINavigationController(rootViewController: ViewController)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true, completion: nil)
+    }
+
+    @objc func button2Tapped(_ sender: UIButton) {
+        print("Button 2 tapped")
+        let ViewController = AddViewController()
+        let navigationController = UINavigationController(rootViewController: ViewController)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true, completion: nil)    }
+
+    @objc func button3Tapped(_ sender: UIButton) {
+        print("Button 3 tapped")
+        let ViewController = AppleLoginViewController()
+        let navigationController = UINavigationController(rootViewController: ViewController)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true, completion: nil)    }
+}
