084ddb92caee1166def9664756755d16e50b8279
hyungjin0105 <hyungjin015@handong.ac.kr>
2024-01-02 14:18:28 +0900

    추가화면 테이블 완료

--- Think/Table/GridViewController.swift
+++ Think/Table/GridViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class GridViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-
+    
     // MARK: - Properties
     let rectangleBox = UIView()
     var gridColumns: Int
@@ -10,7 +10,7 @@ class GridViewController: UIViewController, UICollectionViewDelegate, UICollecti
     let buttonOne = UIButton(type: .system)
     let buttonTwo = UIButton(type: .system)
     var collectionView: UICollectionView!
-
+    
     // MARK: - Initializer
     init(rows: Int, columns: Int) {
         self.gridRows = rows
@@ -18,85 +18,92 @@ class GridViewController: UIViewController, UICollectionViewDelegate, UICollecti
         self.boxes = Array(repeating: false, count: rows * columns)
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
-
+    
     // MARK: - UI Configuration
     private func configureUI() {
         view.backgroundColor = .systemBackground
         setupRectangleBox()
         setupCollectionView()
-
+        
         if gridColumns % 2 == 0 {
             setupButtons()
         }
         setupConstraints()
-
+        
     }
-
+    
     private func setupRectangleBox() {
         rectangleBox.backgroundColor = .red
         rectangleBox.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(rectangleBox)
     }
-
+    
+    
     private func setupButtons() {
         configureButton(buttonOne, title: "One")
+        buttonOne.addTarget(self, action: #selector(adjustSpacingForOneButton), for: .touchUpInside)
+        
         configureButton(buttonTwo, title: "Two")
+        buttonTwo.addTarget(self, action: #selector(adjustSpacingForTwoButton), for: .touchUpInside)
+    }
+    
+    
+    @objc private func adjustSpacingForOneButton() {
+        if let layout = collectionView.collectionViewLayout as? CustomGridLayout {
+            layout.useAlternatingSpacing = false
+            collectionView.collectionViewLayout.invalidateLayout()
+        }
     }
 
+    @objc private func adjustSpacingForTwoButton() {
+        if let layout = collectionView.collectionViewLayout as? CustomGridLayout {
+            // Toggle alternating spacing
+            layout.useAlternatingSpacing = true
+            // Invalidate the layout to apply changes
+            collectionView.collectionViewLayout.invalidateLayout()
+        }
+    }
+    
     private func configureButton(_ button: UIButton, title: String) {
         button.setTitle(title, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(button)
     }
-
+    
     private func setupCollectionView() {
-        let layout = UICollectionViewFlowLayout()
-        
-        let cellSize: CGFloat = 30
-        let basespacing: CGFloat = 2
-        let spacenum = CGFloat(gridColumns) - 1
-        let totalCellWidth = (cellSize * 8) + ((cellSize/2) * 7)
-//        let totalCellWidth = (cellSize * CGFloat(gridColumns) + (basespacing * spacenum))
-        let dynamicSpacing = calculateDynamicSpacing(totalWidth: totalCellWidth, columns: gridColumns, cellsize: cellSize)
-        let totalCellHeight = (cellSize * CGFloat(gridRows)) + (basespacing * CGFloat(gridRows - 1))
-        
-        // Calculate dynamic spacing based on the number of columns
-//        let baseSpacing = 5
-//        let spacingIncrement = 10
-//        let columnDifference = max(0, 8 - gridColumns)
-        layout.minimumInteritemSpacing = dynamicSpacing
-        layout.minimumLineSpacing = 2
-
+        let layout = CustomGridLayout(columns: gridColumns)
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.backgroundColor = .white
         view.addSubview(collectionView)
+    
     }
-
+    
+    
     private func setupConstraints() {
+        guard let layout = collectionView.collectionViewLayout as? CustomGridLayout else {
+            return
+        }
+
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-
-        let cellSize: CGFloat = 30
-        let basespacing: CGFloat = 2
-        let spacenum = CGFloat(gridColumns) - 1
-        let totalCellWidth = (cellSize * 8) + ((cellSize/2) * 7)
-//        let totalCellWidth = (cellSize * CGFloat(gridColumns) + (basespacing * spacenum))
-        let dynamicSpacing = calculateDynamicSpacing(totalWidth: totalCellWidth, columns: gridColumns, cellsize: cellSize)
-        let totalCellHeight = (cellSize * CGFloat(gridRows)) + (basespacing * CGFloat(gridRows - 1))
-
-
+        
+        // Use properties from CustomGridLayout for totalCellWidth and totalCellHeight
+        let spacenum = CGFloat(layout.gridColumns) - 1
+        let totalCellWidth = (layout.cellSize * CGFloat(layout.gridColumns)) + (layout.baseSpacing * spacenum)
+        let totalCellHeight = (layout.cellSize * CGFloat(gridRows)) + (layout.baseHeight * CGFloat(gridRows - 1))
+        
         NSLayoutConstraint.activate([
             rectangleBox.topAnchor.constraint(equalTo: view.topAnchor, constant: view.bounds.height * (3.5 / 7.0)),
             rectangleBox.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -108,8 +115,8 @@ class GridViewController: UIViewController, UICollectionViewDelegate, UICollecti
             collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             collectionView.bottomAnchor.constraint(equalTo: rectangleBox.topAnchor, constant: -20)
         ])
-
-        if gridColumns % 2 == 0 {
+        
+        if layout.gridColumns % 2 == 0 {
             // Set up constraints for the buttons only if they are added to the view
             NSLayoutConstraint.activate([
                 buttonOne.topAnchor.constraint(equalTo: rectangleBox.bottomAnchor, constant: 20),
@@ -121,25 +128,17 @@ class GridViewController: UIViewController, UICollectionViewDelegate, UICollecti
         }
     }
     
-    private func calculateDynamicSpacing(totalWidth: CGFloat, columns: Int, cellsize: CGFloat) -> CGFloat {
-        let totalSpacing = totalWidth - (cellsize * CGFloat(columns))
-        return totalSpacing / CGFloat(columns - 1)
-    }
-
     // UICollectionViewDataSource methods
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return gridRows * gridColumns
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = UIColor.blue // All cells are highlighted
+        cell.backgroundColor = UIColor.systemYellow // All cells are highlighted
+        cell.layer.cornerRadius = 10
+        cell.layer.masksToBounds = true
         return cell
     }
-
-    // UICollectionViewDelegateFlowLayout methods
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 30, height: 30)
-    }
-
+    
 }
